3614c0caccb89f36bbd0b18d71485032fb14f86a
omarbfs <86523956+omarbfs@users.noreply.github.com>
2023-03-23 02:12:03 +0100

    Update 5-sign.c

--- 0x02-functions_nested_loops/5-sign.c
+++ 0x02-functions_nested_loops/5-sign.c
@@ -2,11 +2,11 @@
 
 
 	/**
-	 *print_sign - prints the sign of a number
-	 *@n: the int to check
-	 *Return: 1 and prints + if n is greater than zero
-	 *0 and prints 0 if n is zero
-	 *-1 and prints - if n is less than zero
+	 * prtint_sign - print a sign of a number
+	 *
+	 * @n: The value
+	 *
+	 * Return: 0 or 1 or -1 (Success)
 	 */
 	int print_sign(int n)
 	{
