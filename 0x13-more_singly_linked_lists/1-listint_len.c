6cd020d43b5331d95a3db11939f929bc5ca5970f
omarbfs <bfsomar24@gmail.com>
2023-05-02 17:33:28 -0700

    task1

--- 0x13-more_singly_linked_lists/1-listint_len.c
+++ 0x13-more_singly_linked_lists/1-listint_len.c
@@ -1,23 +1,20 @@
 #include "lists.h"
 
 /**
- * listint_len - elements number return in LL
+ * listint_len - elements number return in linkedlists
  * @h: listint_t type 
  *
- * Return: nodes num 
+ * Return: nodes num
  */
-
 size_t listint_len(const listint_t *h)
 {
 	size_t number = 0;
 
-        do{
-		 return (number);
-	}	 
 	while (h)
 	{
 		number++;
 		h = h->next;
 	}
 
+	return (number);
 }
