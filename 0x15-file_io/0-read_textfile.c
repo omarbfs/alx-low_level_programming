fe3bf76bd488dfb0ce169f1386620739e4fbe06a
omarbfs <86523956+omarbfs@users.noreply.github.com>
2023-09-05 02:27:49 +0200

    Create 0-read_textfile.c

---
+++ 0x15-file_io/0-read_textfile.c
@@ -0,0 +1,29 @@
+#include "main.h"
+#include <stdlib.h>
+
+/**
+ * read_textfile- lire le tecte dans le fichier et printer out.
+ * @filename: le depart de la lecture de fichier
+ * @letters: nombre des lettres qui sont lu
+ * Return: w- le nombre reel des octet qui sont lu
+ * 0 si la fonction etre lu ou n'exist pas.
+ */
+
+ssize_t read_textfile(const char *filename, size_t letters)
+{
+	char *buf;
+	ssize_t fd;
+	ssize_t w;
+	ssize_t t;
+
+	fd = open(filename, O_RDONLY);
+	if (fd == -1)
+		return (0);
+	buf = malloc(sizeof(char) * letters);
+	t = read(fd, buf, letters);
+	w = write(STDOUT_FILENO, buf, t);
+
+	free(buf);
+	close(fd);
+	return (w);
+}
